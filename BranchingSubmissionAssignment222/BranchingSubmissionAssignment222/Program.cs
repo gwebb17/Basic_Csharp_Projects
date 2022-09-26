ffb0a360cd90b8a1b605d063b2f6a37c785da8ec
gwebb17 <gwebb1723@gmail.com>
2022-09-26 14:26:36 -0700

    Completed assignment.

---
+++ BranchingSubmissionAssignment222/BranchingSubmissionAssignment222/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingSubmissionAssignment222
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("What is the package weight?");
+            decimal pweight = Convert.ToDecimal(Console.ReadLine()); //saves input and converts to # format
+
+            if (pweight > 50) //if input greater than 50
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+
+            }
+            else
+            {
+                Console.WriteLine("What is the package width?");
+                decimal pwidth = Convert.ToDecimal(Console.ReadLine()); //saves input from above ? and converts to # format
+                Console.WriteLine("What is the package height?");
+                decimal pheight = Convert.ToDecimal(Console.ReadLine()); //saves input from above ? and converts to #
+                Console.WriteLine("What is the package length?");
+                decimal plength = Convert.ToDecimal(Console.ReadLine()); //saves input from above ? and converts to #
+
+                decimal dimensionTotal = pwidth + pheight + plength; //adds up 3 dimensions saves as decimal
+                if (dimensionTotal > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express");
+                }
+
+                else
+                {
+
+
+
+                    decimal factorDimension = pwidth * pheight * plength; //saves product of dimensions as decimal
+                    decimal finalWeight = pweight * factorDimension; //multiply decimal weight by dimension product
+                    decimal quotientDimension = finalWeight / 100; //divides product by 100 saves as decimal
+                    Console.WriteLine("Your estimated total for shipping this package is: " + String.Format("{0:C}", quotientDimension) + "\nThank You");
+                    //above is the way to format for price and insert that formatted result to console message
+                }
+            }
+            //prints message and divided product in a price aesthetic and thank you message in newline
+
+           
+           
+            Console.ReadLine(); //keeps screen open until closed
+        }
+    }
+}
---
+++ TernaryProject/TernaryProject/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TernaryProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("whats your fav #");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+
+            string result = favNum == 12 ? "you guessed it " : "you didn't guess it";
+            Console.WriteLine(result);
+            //int roomTemperature = 70;
+            //Console.WriteLine("whats your name");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("hi " + name + " what is temperature where you are?");
+            //int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("its exactly room temp");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("its warmer than room temp");
+            //}
+            //else if (roomTemperature > currentTemperature)
+            //{
+            //    Console.WriteLine("its colder than room temp");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("something went wront");
+            //}
+            
+            
+            //int currentTemperature = 80;
+            //int roomTemperature = 70;
+
+            //string comparisonResult = currentTemperature == roomTemperature ? "it is exactly room temp" : "it is not exactly room temp";
+            //Console.WriteLine(comparisonResult);
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("it is exactly room temperature");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("its warmer than roomtemp");
+            //}
+            //else if (roomTemperature > currentTemperature)
+            //{
+            //    Console.WriteLine("room temp is warmer than current");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("it is not exactly room temperature");
+            //}
+            Console.ReadLine();
+        }
+    }
+}
