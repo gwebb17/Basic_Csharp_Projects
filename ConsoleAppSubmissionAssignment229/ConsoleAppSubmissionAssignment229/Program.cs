c10b108d50e1731a2b0b0a2272a947b86eab06dd
gwebb17 <gwebb1723@gmail.com>
2022-09-27 09:59:49 -0700

    Completed assignment

---
+++ ConsoleAppSubmissionAssignment229/ConsoleAppSubmissionAssignment229/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSubmissionAssignment229
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Name the largest country in the Middle East by area");
+            string guess = Console.ReadLine();
+            bool correctAnswer = guess == "Saudi Arabia"; //returns true or false if guess == correct country
+
+            do
+            {
+                switch (guess)
+                {
+                    case "Iraq":
+                        Console.WriteLine("Sorry wrong guess. Try again.");
+                        Console.WriteLine("Name the largest country in the Middle East by area");
+                        guess = Console.ReadLine(); //don't need to specify data type when redefining guess subsequent times
+                        break;
+                    case "Iran":
+                        Console.WriteLine("Sorry wrong guess. Try again.");
+                        Console.WriteLine("Name the largest country in the Middle East by area");
+                        guess = Console.ReadLine();
+                        break;
+                    case "India":
+                        Console.WriteLine("That one's in Asian. Try Again.");
+                        Console.WriteLine("Name the largest country in the Middle East by area");
+                        guess = Console.ReadLine();
+                        break;
+                    case "Saudi Arabia":
+                        Console.WriteLine("You are correct.");
+                        correctAnswer = true; //set bool to true if correct in order to exit loop 
+                        break;
+                    default:
+                        Console.WriteLine("Here's a hint; there are two words in the country name");
+                        Console.WriteLine("Name the largest country in the Middle East by area");
+                        guess = Console.ReadLine();
+                        break;
+
+                }
+            }
+
+            while (!correctAnswer); //this says do the above switch statement only while correctAnswer is NOT TRUE
+            
+
+
+            Console.WriteLine("Enter an even number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            while (number % 2 != 0)
+            {
+                Console.WriteLine("You entered an odd number, try again");
+                Console.WriteLine("Enter an even number:");
+                number = Convert.ToInt32(Console.ReadLine());
+                break;
+
+
+            }
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("You did it");
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ SwitchChallenge226/SwitchChallenge226/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchChallenge226
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Guess a #");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12; //this returns true or false (does number ==12? if yes then TRUE
+
+
+            do
+            {
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("you guessed 62");
+                        Console.WriteLine("Guess a #");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("you guessed 29");
+                        Console.WriteLine("Guess a #");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("you guessed 55");
+                        Console.WriteLine("Guess a #");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("guessed 12 yay");
+                        isGuessed = true; //set isGuessed to true when correct # guessed
+                        break;
+                    default:
+                        Console.WriteLine("you are wrong");
+                        Console.WriteLine("Guess a #");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed); //this says while isGuessed is Not True. once its true the do statement exits
+            
+            Console.ReadLine();
+                
+            
+           
+        }
+    }
+}
