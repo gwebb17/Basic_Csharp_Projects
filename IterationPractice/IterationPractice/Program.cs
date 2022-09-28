75547695b4be0c671fdc6115e2b70c4418658e33
gwebb17 <gwebb1723@gmail.com>
2022-09-28 10:03:32 -0700

    updated

---
+++ IterationPractice/IterationPractice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        List<int> testScores = new List<int>() { 98, 48, 85, 77, 38, 99 };
+        List<int> passingScores = new List<int>();  //blank list to have passing values from above list inserted into (like a temp)
+
+        foreach (int score in testScores)
+        {
+            if (score > 85)
+            {
+                passingScores.Add(score);
+            }
+        }
+
+        Console.WriteLine(passingScores.Count);
+            
+        Console.ReadLine();
+        }
+    }
+
