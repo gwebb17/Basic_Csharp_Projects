e25793b51ba5866a3b3e725e2b67a783fab269ac
gwebb17 <gwebb1723@gmail.com>
2022-09-30 12:39:30 -0700

    completed assignment

---
+++ MethodClassAssignment281/MethodClassAssignment281/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment281
+{
+    public class Class1
+    {
+        public void Method1(int x, int y)
+        {
+            int result1 = x * 16;   //math operation for x
+            Console.WriteLine(y);   //print to console value of y
+
+        }
+    }
+}
---
+++ MethodClassAssignment281/MethodClassAssignment281/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment281
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1Object = new Class1(); //create new object
+            class1Object.Method1(7, 8);  ///WHEN USING VOID METHOD WE JUST CALL METHOD LIKE THIS
+
+            class1Object.Method1(x:1, y:98);  //TO GIVE VALUES FOR PARAMS USE : NOT =
+            Console.ReadLine();
+        }
+    }
+}
