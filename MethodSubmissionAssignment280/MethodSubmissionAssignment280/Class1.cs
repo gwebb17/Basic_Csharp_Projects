e21749cc21074caccecb5ed42451ab1670adf3dd
gwebb17 <gwebb1723@gmail.com>
2022-09-30 12:08:23 -0700

    Completed step 280

---
+++ MethodSubmissionAssignment280/MethodSubmissionAssignment280/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment280
+{
+    class Class1
+    {
+        public int Method1(int x, int y = 1)
+        {
+            int method1resultx = x * 2 + y;
+            return method1resultx;
+        }
+    }
+}
---
+++ MethodSubmissionAssignment280/MethodSubmissionAssignment280/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment280
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1Object = new Class1(); //instantiate object 
+
+            Console.WriteLine("Please enter one number");
+            int answer1 = Convert.ToInt32(Console.ReadLine()); //save first entered number as int var
+            Console.WriteLine("Enter an optional second number");
+
+            try   //if we don't enter a second param we will get errors, so in order to have a solution we use a try/catch
+            {
+                int answer2 = Convert.ToInt32(Console.ReadLine());   //this trys to run program as if user entered both params
+                int mainResult1 = class1Object.Method1(answer1, answer2);
+                Console.WriteLine(mainResult1);
+            }
+            catch  //in the event user doesn't enter 2nd param (y), we will simply run Method1 without 2nd param and display that result 
+            {
+                int mainResult1 = class1Object.Method1(answer1);
+                Console.WriteLine(mainResult1);
+                
+            }
+
+
+          
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
