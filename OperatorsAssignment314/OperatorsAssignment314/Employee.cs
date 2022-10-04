1d4cc0434d4c66f0745eade90dfcd5827f2dff3a
gwebb17 <gwebb1723@gmail.com>
2022-10-04 09:04:31 -0700

    Completed assignment

---
+++ OperatorsAssignment314/OperatorsAssignment314/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment314
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //INSTEAD of method name we use keyword 'operator' followed by type of operator in method definitions. These are automatically inherited by objects
+        //of this class so thats why we don't need to name them when calling in program.cs (i think)
+
+        public static bool operator == (Employee a, Employee b)  //needs to be static i think, bool because our results are either TRUE or NOT TRUE
+        {
+            return (a.Id == b.Id);
+
+        }
+
+        public static bool operator != (Employee a, Employee b)   //method2 correlating to opposite case (!=) we need both == and != method present for comparison operator overloading
+        {
+            return (a.Id != b.Id);
+        }
+    }
+}
---
+++ OperatorsAssignment314/OperatorsAssignment314/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment314
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee a = new Employee();  //instantiate class object
+            a.Id = 1;
+            a.FirstName = "Reggie";
+            a.LastName = "Reginold";
+
+            Employee b = new Employee(); //instantiate class object #2 for comparison
+            b.Id = 2;
+            b.FirstName = "Paul";
+            b.LastName = "Wall";
+
+            //uneccessary array assignment keeping just in case proves useful somewhere
+            //int[] workerArray = new int[1];  //create an array to hold values for ID props
+            //workerArray[0] = a.Id;  //Employee a
+            //workerArray[1] = b.Id;  //Employee b
+
+            //Console.WriteLine(workerArray);
+
+            Console.WriteLine(a.Id == b.Id);  //now run 'method' (==) and since == has been redefined in Employee.cs its action will be applied to program when using == or !=
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
